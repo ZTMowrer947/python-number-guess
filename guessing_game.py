0c13eed6693eb13da439b41f5c9226881bb74a2e
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-04-02 10:13:28 -0400

    Implement psuedocode for meeting expectations

--- guessing_game.py
+++ guessing_game.py
@@ -9,30 +9,32 @@ import random
 
 def start_game():
     """This function runs the entire game as described in the module help."""
-    #   print intro message
-    #   generate random number between 1 and 10
-    #   set random_number to result
-    #   set guess_counter to 1
-    #   while true
-    #       get guess from user
-    #       try
-    #           cast guess to integer
-    #       handle error in casting
-    #          print "Sorry, that isn't a valid guess. Your guess must be a number."
-    #          continue to next iteration
-    #       endtry
-    #       if guess is equal to random_number
-    #           print "You got it! The number was {random_number}!"
-    #           print "It took you {guess_counter} guesses to get it."
-    #           exit loop
-    #       else if guess is greater than random_number
-    #           print "{guess} is too high. Guess lower!"
-    #       else guess must be less than random_number
-    #           print "{guess} is too low. Guess higher!"
-    #       endif
-    #       increment guess_counter by 1
-    #   endwhile
-    #   print closing message
+    print("Welcome to the Number guessing game!")
+
+    random_number = random.randint(1, 10)
+
+    guess_counter = 1
+
+    while True:
+        guess = input("\nPick a number between 1 and 10: ")
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Sorry, that isn't a valid guess. Your guess must be a number.")
+            continue
+
+        if guess == random_number:
+            print("You got it! The number was {}!".format(random_number))
+            print("It took you {} guess(es) to get it.".format(guess_counter))
+            break
+        elif guess > random_number:
+            print("{} is too high. Guess lower!".format(guess))
+        else:
+            print("{} is too low. Guess higher!".format(guess))
+
+        guess_counter += 1
+
+    print("\nThank you for playing!")
 
 
 start_game()
